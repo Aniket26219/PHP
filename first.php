80fb743d44a6661a5e755d3102e463d745c8e6b9
ANIKET <aniket98shahane@gmail.com>
2020-10-28 11:26:46 +0530

    knowing variables in PHP

--- first.php
+++ first.php
@@ -1,14 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <h1>First Progeam</h1>
-</head>
+<html> 
 <body>
+
     <?php
-    echo "My First PHP Program"
+    $t = "Coding";
+    echo "I love $t";
     ?>
+    
 </body>
 </html>
