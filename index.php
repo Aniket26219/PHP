606393c75a87d2434bfec16808b3376d524ca351
ANIKET <aniket98shahane@gmail.com>
2020-10-28 12:31:50 +0530

    global and local scope and variables

--- index.php
+++ index.php
@@ -3,11 +3,14 @@
 <body>
 <?php
     $txt = "Sample text";
-    $a = 5;
-    $b = 10;
-    echo "This is " . $txt . "<br>";
-    echo "This is $txt<br>";
-    echo $a + $b
+    function test(){
+        echo "Some $txt"; 
+        //This one is not showing in output cuz it is in local scope which does not accept global variables it only accepts local variables
+    }
+    test();
+    echo "Some $txt"
+    //This one will be shown on in the output cuz it is in global scope which accepts only global variables
+    //From current understanding of mine says that global variables are accepted only in global scopes only and local variables accepted by local scope only.
 ?>
 </body>
 </html>
