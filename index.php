0d157e1481d140b8fd21d1f54750595697cd0c01
ANIKET <aniket98shahane@gmail.com>
2020-10-29 13:22:14 +0530

    Practicing the object in PHP

--- index.php
+++ index.php
@@ -2,22 +2,26 @@
 <html> 
 <body>
 <?php
-    class Mobiles{
-        public $brand;
-        public $price;
-        public function __construct($br, $pr){
-            $this->brand = $br;
-            $this->price = $pr;
+    class Candidates{
+        public $name;
+        public $email;
+        public $mobno;
+        public $location;
+        public function __construct($nme, $emal, $mobileno, $loc){
+            $this->name = $nme;
+            $this->email = $emal;
+            $this->mobno = $mobileno;
+            $this->location = $loc;
         }
         public function show(){
-            return "Mobile Company is " . $this->brand . " and price is " . $this->price;
+            return "Candidate Name :- " . $this->name . " <br>Candidate Email :- " . $this->email . "<br>Candidate Mobile NO :- " . $this->mobno . "<br>Candidate's Current Location :- " . $this->location;
         }
     }
-    $iphone = new Mobiles("Iphone" , 80000);
-    echo $iphone -> show();
+    $Aniket = new Candidates("Aniket", "aniketsample@gmail.com", 9999999999, "Nanded");
+    echo $Aniket -> show();
     echo "<br>";
-    $blackberry = new Mobiles("BlackBerry" , 50000);
-    echo $blackberry -> show();
+    $Sagar = new Candidates("Sagar", "sagarsample@gmail.com", 9999999999, "Pune");
+    echo $Sagar -> show();
 ?>
 </body>
 </html>
