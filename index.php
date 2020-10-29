74cd622fe3bd740a40ec25be3c77ff32daca65c7
ANIKET <aniket98shahane@gmail.com>
2020-10-29 14:04:24 +0530

    PHP String : strlen()

--- index.php
+++ index.php
@@ -2,11 +2,7 @@
 <html> 
 <body>
 <?php
-    $a = "Hello";
-    $a = null;
-    echo "$a<br>";
-    var_dump($a);
+    echo strlen("My name is Aniket")
 ?>
 </body>
 </html>
-<!-- when we make a variable data type to NULL the value we gave it before making it null will also be gone. For above example we'll get output only null not Hello cuz we have made it null -->
