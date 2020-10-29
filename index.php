4f1b32594300072daf42d8ded1b874e05fb17ea9
ANIKET <aniket98shahane@gmail.com>
2020-10-29 15:06:25 +0530

    PHP Math : min() & max()

--- index.php
+++ index.php
@@ -2,18 +2,9 @@
 <html> 
 <body>
 <?php
-    $a = 423.3;
-    $n = "3243.24";
-    $an = 32.4;
-    $a_cast = (int)$a;
-    $n_cast = (integer)$n;
-    $an_cast = intval($an);
-    echo $a_cast;
+    echo min(23, 34, 23, 34, 84, 3);
     echo "<br>";
-    echo $n_cast;
-    echo "<br>";
-    echo $an_cast;
+    echo max(34, 34,56, 94, 82, 834);
 ?>
 </body>
 </html>
-<!-- Don't give the _cast for variable it's only for my convineance. If we have to cast into integer just put (int)$--/(integer)$-- or intval($--) replace -- the variable to cast -->
