debc078626009c8a27f1f9e42ea971197b90d95b
ANIKET <aniket98shahane@gmail.com>
2020-10-29 14:30:25 +0530

    some changes for infinite checking

--- index.php
+++ index.php
@@ -9,4 +9,5 @@
 ?>
 </body>
 </html>
+<!-- the output will show either INF means infinite otherwise the float number -->
 <!-- important thing to know is that even if 4 * 2.5 is 10, the result is stored as float, because one of the operands is a float -->
