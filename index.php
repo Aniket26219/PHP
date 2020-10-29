7ef64fe84d7d7b7c542fe405c751b3dde714bd57
ANIKET <aniket98shahane@gmail.com>
2020-10-29 12:44:24 +0530

    Datatypes in the PHP

--- index.php
+++ index.php
@@ -2,11 +2,17 @@
 <html> 
 <body>
 <?php
-    $name = "Aniket";
-    $location = "Nanded";
-    $a = 10; $b = 20;
-    print "My name is " . $name . " and I live in " . $location . "<br>";
-    print $a + $b
+    $txt = "Sample Text";
+    $num = 10;
+    $num1 = 10.4;
+    $bolean = true;
+    $mobiles = array("Samsung", "Iphone", "BlackBerry");
+    var_dump($txt);
+    var_dump($num);
+    var_dump($num1);
+    var_dump($bolean);
+    var_dump($mobiles);
 ?>
 </body>
 </html>
+<!-- var_dump($___) this method shows the data type of the variables which is inside. -->
