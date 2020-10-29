f850bbd5c5d9390bc13efe5fe4f546891a9d6c0a
ANIKET <aniket98shahane@gmail.com>
2020-10-29 12:23:05 +0530

    Static variables in PHP

--- index.php
+++ index.php
@@ -2,16 +2,16 @@
 <html> 
 <body>
 <?php
-    $a = 10;
-    $b = 20;
     function test(){
-        // global $a, $b;
-        // $b = $a + $b;
-        $GLOBALS['b'] = $GLOBALS['a'] + $GLOBALS['b']; //another approach
+        static $a = 1;
+        echo "$a <br>";
+        $a++;
     }
     test();
-    echo "$b from another approach";
-    //From this I have understanded that if we do any operation on the global variables in a function it will affect it out of it's local scope only when we call that function otherwise it will not affect it. If we don't call the function the the value will be 20 and if we call then the value will be 30.
+    test();
+    test();
 ?>
 </body>
 </html>
+
+<!-- normally local variables get deleted after the execution is completed but static variables don't get deleted rather than that it's value is stored. When we again call it the value gets added with the newer one. Like above if we call the test once the value will be one if we call it twice the value will be 2 cuz the first value is not get deleted and when we call it again the value gets added with second and that's why it shows us 2. -->
