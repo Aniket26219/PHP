2333a20c659354ae3df67877090513b4c0377eb5
ANIKET <aniket98shahane@gmail.com>
2020-10-29 14:17:17 +0530

    PHP Numbers : integer checking methods

--- index.php
+++ index.php
@@ -2,7 +2,14 @@
 <html> 
 <body>
 <?php
-    echo str_replace("Aniket", "Aniket Shahane", "My name is Aniket")
+    $a = 24;
+    var_dump(is_int($a));
+    echo "<br>";
+    var_dump(is_integer($a));
+    echo "<br>";
+    var_dump(is_long($a))
 ?>
 </body>
 </html>
+<!-- above three methods is_int(), is_integer() and is_long() all are to check whether the given number is integer or not -->
+<!-- important thing to know is that even if 4 * 2.5 is 10, the result is stored as float, because one of the operands is a float -->
