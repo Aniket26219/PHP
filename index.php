9b381728c6a8eec9618ae22937d1a89e59386e3b
ANIKET <aniket98shahane@gmail.com>
2020-10-29 12:16:55 +0530

    Another approach to access globl var in locl scope

--- index.php
+++ index.php
@@ -5,11 +5,12 @@
     $a = 10;
     $b = 20;
     function test(){
-        global $a, $b;
-        $b = $a + $b;
+        // global $a, $b;
+        // $b = $a + $b;
+        $GLOBAL['b'] = $GLOBAL['a'] + $GLOBAL['b']; //another approach
     }
     test();
-    echo $b;
+    echo "$b from another approach";
     //From this I have understanded that if we do any operation on the global variables in a function it will affect it out of it's local scope only when we call that function otherwise it will not affect it. If we don't call the function the the value will be 20 and if we call then the value will be 30.
 ?>
 </body>
