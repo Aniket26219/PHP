57aed4685f99f73369aa594885774400aea1cc3e
ANIKET <aniket98shahane@gmail.com>
2020-10-31 14:52:20 +0530

    PHP Do while loop

--- index.php
+++ index.php
@@ -2,12 +2,13 @@
 <html> 
 <body>
 <?php
-    $a = 0;
-    while ($a <= 100) {
-        echo "Count is $a";
-        $a+=10;
-        echo "<br>";
-    }
+$x = 0;
+
+do {
+    $x++;
+    echo "The number is: $x <br>";
+} while ($x <= 5);
 ?>
 </body>
 </html>
+<!-- As every body know the do while first it will execute the code then checks the condition. For above code the o/p will from 0-6. Now why 6 is bcuz first it will execute the above loop while the value is still 6 then check condition and stop executing the code. -->
