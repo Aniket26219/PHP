5420630aae237a988c252fdcaed3584acc40170f
ANIKET <aniket98shahane@gmail.com>
2020-10-31 15:10:42 +0530

    PHP Using the Strict

--- index.php
+++ index.php
@@ -1,14 +1,7 @@
-<!DOCTYPE html>
-<html> 
-<body>
 <?php
-for ($x = 0; $x < 10; $x++) {
-    if ($x == 4) {
-    continue;
-    }
-    echo "The number is: $x <br>";
+    declare(strict_types=1);
+    function addNumbers(int $a, int $b) {
+    return $a + $b;
 }
+    echo addNumbers(5, "5 days");;
 ?>
-</body>
-</html>
-<!-- when the specified condition or conditions match it will skip that from an iteration for above example value 4 will be skipped. -->
