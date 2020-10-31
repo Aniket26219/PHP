9ac224597aa3e3bb4a4ab9938b927ec3c37f8363
ANIKET <aniket98shahane@gmail.com>
2020-10-31 14:48:49 +0530

    PHP While Loop

--- index.php
+++ index.php
@@ -2,21 +2,12 @@
 <html> 
 <body>
 <?php
-$favcolor = "orange";
-
-switch ($favcolor) {
-    case "red":
-    echo "Your favorite color is $favcolor!";
-    break;
-    case "blue":
-    echo "Your favorite color is $favcolor!";
-    break;
-    case "green":
-    echo "Your favorite color is $favcolor!";
-    break;
-    default:
-    echo "Your favorite color is neither red, blue, nor green but it is $favcolor!";
-}
+    $a = 0;
+    while ($a <= 100) {
+        echo "Count is $a";
+        $a+=10;
+        echo "<br>";
+    }
 ?>
 </body>
 </html>
