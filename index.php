7c5bbd61f8e5f7e441e5065babb24297bab190c5
ANIKET <aniket98shahane@gmail.com>
2020-10-31 14:44:15 +0530

    PHP Switch Case

--- index.php
+++ index.php
@@ -2,9 +2,21 @@
 <html> 
 <body>
 <?php
-    $a = "Aniket";
-    $n = "Nanded";
-    echo $a . " lives in " . $n;
+$favcolor = "orange";
+
+switch ($favcolor) {
+    case "red":
+    echo "Your favorite color is $favcolor!";
+    break;
+    case "blue":
+    echo "Your favorite color is $favcolor!";
+    break;
+    case "green":
+    echo "Your favorite color is $favcolor!";
+    break;
+    default:
+    echo "Your favorite color is neither red, blue, nor green but it is $favcolor!";
+}
 ?>
 </body>
 </html>
