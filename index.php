766d141cfe46629705d49367a14317f3e625641a
ANIKET <aniket98shahane@gmail.com>
2020-11-02 11:58:43 +0530

    PHP Array count() function

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <body>
 <?php
 $n = array("Iphone", "Samsung", "BlackBerry");
-echo $n[0] . ", " . $n[1] . " and " . $n[2] . " are some famous mobile companies."
+echo count($n);
 ?>
 </body>
 </html>
