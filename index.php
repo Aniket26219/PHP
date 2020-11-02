16bcf309b90d992571699cb290aebdcbadf7d436
ANIKET <aniket98shahane@gmail.com>
2020-11-02 11:57:43 +0530

    PHP Array

--- index.php
+++ index.php
@@ -2,13 +2,8 @@
 <html> 
 <body>
 <?php
-function sum(&$val) {
-    $val += 10;
-}
-$n = 10;
-sum($n);
-echo $n;
+$n = array("Iphone", "Samsung", "BlackBerry");
+echo $n[0] . ", " . $n[1] . " and " . $n[2] . " are some famous mobile companies."
 ?>
 </body>
 </html>
-<!-- To turn a function argument which $val into a reference which is $n we are passing to the sum function, the & operator is used -->
